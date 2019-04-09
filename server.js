5574a1c537f93546438563c1d3c587bef78bd2b8
Uriel <uriel.mexicano35@gmail.com>
2019-04-09 01:21:42 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+var app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.port || 3000;
+
+//middlewares una instrucción o un callback que se va a ejecutar siempre, no importa la url que el usuario pida
+app.use(express.static(__dirname + '/public'));
+//Express hbs engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Uriel',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
